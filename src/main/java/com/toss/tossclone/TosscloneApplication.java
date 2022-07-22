ee15405a8b9e6feb24aa6d56a53f613851285196
leejh7 <777joonho@kookmin.ac.kr>
2022-07-22 15:45:03 +0900

    First Commit

---
+++ src/main/java/com/toss/tossclone/TosscloneApplication.java
@@ -0,0 +1,13 @@
+package com.toss.tossclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TosscloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TosscloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toss/tossclone/TosscloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toss.tossclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TosscloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
